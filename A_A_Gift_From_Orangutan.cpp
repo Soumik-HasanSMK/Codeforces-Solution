f935bd0e113bea697324eff3a2a5b1a383c56488
Soumik Hasan <144214624+Soumik-HasanSMK@users.noreply.github.com>
2024-10-19 23:40:40 +0600

    Add files via upload

---
+++ A_A_Gift_From_Orangutan.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define db double
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+        }
+        sort(v.begin(), v.end());
+        int mx = v[n - 1];
+        int mn = v[0];
+        cout << (n - 1) * (mx - mn) << endl;
+    }
+    return 0;
+}
